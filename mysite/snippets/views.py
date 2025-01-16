741c6445ebe0fdf60d42f58e56ff8077e052fac2
sanavalos <santiago.avalos@outlook.com>
2025-01-15 21:30:44 -0300

    feat: add structure snippets app

---
+++ mysite/snippets/views.py
@@ -0,0 +1,17 @@
+from django.views.decorators.csrf import csrf_exempt
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
